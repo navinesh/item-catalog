2a5a2aa49137f9641c79538e92a143a5f9bb665b
navinesh <navinesh.chand@gmail.com>
2015-11-13 17:59:27 +1300

    add sqlalchemy imports

--- vagrant/catalog/application.py
+++ vagrant/catalog/application.py
@@ -1,6 +1,15 @@
 from flask import Flask
 app = Flask(__name__)
 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Categories, Items
+
+engine = create_engine('')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
 @app.route('/')
 def HelloWorld():
     return "Hello World!"

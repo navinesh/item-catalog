33235452db52782b6c4d6d140575868cbafcf41d
navinesh <navinesh.chand@gmail.com>
2015-11-15 20:50:39 +1300

    add serialize functions

--- vagrant/catalog/database_setup.py
+++ vagrant/catalog/database_setup.py
@@ -1,3 +1,4 @@
+import os
 import sys
 
 from sqlalchemy import Column, ForeignKey, Integer, String
@@ -8,13 +9,38 @@ from sqlalchemy import create_engine
 Base = declarative_base()
 
 class Categories(Base):
-    __tablename__ = 'categories'
+    __tablename__ = 'categories'  # representation of table inside database
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100), nullable=False)
+
+    @property
+    def serialize(self):
+        return {
+        'id' : self.id,
+        'name' : self.name,
+        }
 
 
 class Items(Base):
-    __tablename__ = 'items'
+    __tablename__ = 'items'  # representation of table inside database
 
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100), nullable=False)
+    description = Column(String(250), nullable=False)
+    categories_id = Column(Integer, ForeignKey('categories.id'))
+    categories = relationship(Categories)
 
-engine = create_engine('')
+    @property
+    def serialize(self):
+        return {
+        'id' : self.id,
+        'name' : self.name,
+        'description' : self.description,
+        'categories_id' : self.categories_id
+        }
 
+engine = create_engine('postgresql:///itemcatalog')
+
+# add class as new tables in database
 Base.metadata.create_all(engine)

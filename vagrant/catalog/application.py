3755981378a343814d021af42287ffab65976101
navinesh <navinesh.chand@gmail.com>
2015-11-15 20:49:00 +1300

    add show and new categories function

--- vagrant/catalog/application.py
+++ vagrant/catalog/application.py
@@ -1,20 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for
 app = Flask(__name__)
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Categories, Items
 
-engine = create_engine('')
+engine = create_engine('postgresql:///itemcatalog')
 Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 @app.route('/')
-def HelloWorld():
-    return "Hello World!"
+@app.route('/categories')
+def showCategories():
+    """displays all categories"""
+    categories = session.query(Categories).all()
+    return render_template('categories.html', categories=categories)
+
+@app.route('/categories/new/', methods=['GET', 'POST'])
+def newCategories():
+    """add new category to database"""
+    if method == 'POST':
+        newCategories = Categories(name=request.form['name'])
+        session.add(newCategories)
+        session.commit()
+    return redirect(url_for('showCategories'))    
+
 
 
 if __name__ == '__main__':
     app.debug = True
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=8000)

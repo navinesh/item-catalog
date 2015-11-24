458e47f8463ab5072a342ed300589cd88ca69b26
navinesh <navinesh.chand@gmail.com>
2015-11-24 18:26:16 +1300

    change relationship cascade

--- vagrant/catalog/database_setup.py
+++ vagrant/catalog/database_setup.py
@@ -1,15 +1,15 @@
-import os
 import sys
 
 from sqlalchemy import Column, ForeignKey, Integer, String, Text
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, backref
 from sqlalchemy import create_engine
 
 Base = declarative_base()
 
-class Categories(Base):
-    __tablename__ = 'categories'  # representation of table inside database
+
+class Category(Base):
+    __tablename__ = 'category'  # representation of table inside database
 
     id = Column(Integer, primary_key=True)
     name = Column(String(100), nullable=False)
@@ -17,31 +17,33 @@ class Categories(Base):
     @property
     def serialize(self):
         return {
-        'id' : self.id,
-        'name' : self.name,
+            'id': self.id,
+            'name': self.name,
         }
 
-class Items(Base):
-    __tablename__ = 'items'  # representation of table inside database
+
+class Item(Base):
+    __tablename__ = 'item'  # representation of table inside database
 
     id = Column(Integer, primary_key=True)
     name = Column(String(100), nullable=False)
     description = Column(String(250), nullable=False)
     url = Column(Text, nullable=True, unique=True)
-    category_id = Column(Integer, ForeignKey('categories.id'))
-    categories = relationship(Categories)
+    category_id = Column(Integer, ForeignKey('category.id'))
+    category = relationship(
+        Category, backref=backref("items", cascade="all, delete-orphan"))
 
     @property
     def serialize(self):
         return {
-        'id' : self.id,
-        'name' : self.name,
-        'description' : self.description,
-        'category_id' : self.category_id,
-        'url' : self.url,
+            'id': self.id,
+            'name': self.name,
+            'description': self.description,
+            'category_id': self.category_id,
+            'url': self.url,
         }
 
 engine = create_engine('postgresql:///itemcatalog')
 
-# add class as new tables in database
+# add class as new table in database
 Base.metadata.create_all(engine)

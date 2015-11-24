efbb826543fac721e71e4af7f087712298c15568
navinesh <navinesh.chand@gmail.com>
2015-11-24 18:26:33 +1300

    change table names

--- vagrant/catalog/application.py
+++ vagrant/catalog/application.py
@@ -5,7 +5,7 @@ from werkzeug import secure_filename
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from database_setup import Base, Categories, Items
+from database_setup import Base, Category, Item
 
 # directory to store images
 UPLOAD_FOLDER = 'uploads/'
@@ -25,15 +25,24 @@ session = DBSession()
 @app.route('/categories/')
 def showCategories():
     """displays all categories in database"""
-    categories = session.query(Categories).all()
-    return render_template('categories.html', categories=categories)
+    categories = session.query(Category).all()
+    items = session.query(Item).all()
+    return render_template('categories.html', categories=categories, items=items)
+
+
+@app.route('/home/')
+def shows():
+    """displays all categories in database"""
+    categories = session.query(Category).all()
+    items = session.query(Item).all()
+    return render_template('home.html', categories=categories, items=items)
 
 
 @app.route('/categories/new/', methods=['GET', 'POST'])
 def newCategory():
     """add new category to the database"""
     if request.method == 'POST':
-        newCategory = Categories(name=request.form['name'])
+        newCategory = Category(name=request.form['name'])
         session.add(newCategory)
         session.commit()
         return redirect(url_for('showCategories'))
@@ -44,7 +53,7 @@ def newCategory():
 @app.route('/categories/<int:category_id>/edit/', methods=['GET', 'POST'])
 def editCategory(category_id):
     """edit category"""
-    editCategory = session.query(Categories).filter_by(id=category_id).one()
+    editCategory = session.query(Category).filter_by(id=category_id).one()
     if request.method == 'POST':
         if request.form['name']:
             editCategory.name = request.form['name']
@@ -58,7 +67,7 @@ def editCategory(category_id):
 @app.route('/categories/<int:category_id>/delete/', methods=['GET', 'POST'])
 def deleteCategory(category_id):
     """delete category"""
-    deleteCategory = session.query(Categories).filter_by(id=category_id).one()
+    deleteCategory = session.query(Category).filter_by(id=category_id).one()
     if request.method == 'POST':
         session.delete(deleteCategory)
         session.commit()
@@ -84,8 +93,8 @@ def uploaded_image(filename, category_id, items_id):
 @app.route('/categories/<int:category_id>/items/')
 def showItems(category_id):
     """displays all items in each category"""
-    category = session.query(Categories).filter_by(id=category_id).first()
-    items = session.query(Items).filter_by(category_id=category_id)
+    category = session.query(Category).filter_by(id=category_id).first()
+    items = session.query(Item).filter_by(category_id=category_id)
     return render_template('items.html', category=category, items=items)
 
 
@@ -98,14 +107,16 @@ def allowed_file(filename):
 @app.route('/categories/<int:category_id>/items/new/', methods=['GET', 'POST'])
 def newItem(category_id):
     """add new item for a particular category in the database"""
-    category = session.query(Categories).filter_by(id=category_id).first()
+    category = session.query(Category).filter_by(id=category_id).first()
     if request.method == 'POST':
         file = request.files['file']  # check if an image was posted
         if file and allowed_file(file.filename):  # check extension
             filename = secure_filename(file.filename)  # return secure version
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        newItem = Items(name=request.form['name'], category_id=category_id,
-                        description=request.form['description'], url=filename)
+        else:
+            filename = None
+        newItem = Item(name=request.form['name'], category_id=category_id,
+                       description=request.form['description'], url=filename)
         session.add(newItem)
         session.commit()
         return redirect(url_for('showItems', category_id=category_id))
@@ -118,8 +129,8 @@ def newItem(category_id):
            methods=['GET', 'POST'])
 def editItem(category_id, items_id):
     """edit item of a particular category"""
-    categories = session.query(Categories).all()
-    editItem = session.query(Items).filter_by(id=items_id).one()
+    categories = session.query(Category).all()
+    editItem = session.query(Item).filter_by(id=items_id).one()
     if request.method == 'POST':
         if request.form['name']:
             editItem.name = request.form['name']
@@ -142,7 +153,7 @@ def editItem(category_id, items_id):
            methods=['GET', 'POST'])
 def deleteItem(category_id, items_id):
     """delete item of a particular category"""
-    deleteItem = session.query(Items).filter_by(id=items_id).one()
+    deleteItem = session.query(Item).filter_by(id=items_id).one()
     if request.method == 'POST':
         session.delete(deleteItem)
         session.commit()
@@ -154,26 +165,26 @@ def deleteItem(category_id, items_id):
 
 @app.route('/catalog.json/')
 def catalogJSON():
-    categories = session.query(Categories).all()
+    categories = session.query(Category).all()
     items = session.query(Items).all()
     return jsonify(Categories=[c.serialize for c in categories], Items=[i.serialize for i in items])
 
 
 @app.route('/categories/json/')
 def categoriesJSON():
-    categories = session.query(Categories).all()
+    categories = session.query(Category).all()
     return jsonify(Categories=[c.serialize for c in categories])
 
 
 @app.route('/categories/<int:category_id>/items/json/')
 def categoryItemJSON(category_id):
-    items = session.query(Items).filter_by(category_id=category_id).all()
+    items = session.query(Item).filter_by(category_id=category_id).all()
     return jsonify(Items=[i.serialize for i in items])
 
 
 @app.route('/categories/<int:category_id>/items/<int:items_id>/json/')
 def categoryItemsJSON(category_id, items_id):
-    items = session.query(Items).filter_by(id=items_id).one()
+    items = session.query(Item).filter_by(id=items_id).one()
     return jsonify(Items=[items.serialize])
 
 
